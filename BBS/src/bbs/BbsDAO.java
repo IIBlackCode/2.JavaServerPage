773964fcacdff9c89c70621abab45559e5935496
MASTER <MASTER@DESKTOP-2OVON5I>
2020-11-21 16:19:44 +0900

    1. 프로젝트 설치 2. 로그인 페이지 구현 3. 데이터베이스 연동 완료 4. 로그인 기능 구현 5. 회원가입 페이지 구현 6. 회원가입 기능 구현 7. 세션 추가 8. 게시판 메인 페이지 구현 9. 게시판 데이터베이스 구축 10.글쓰기 기능 구현 11.게시판 글 목록 기능 구현 12.게시글 읽기 기능 구현

--- BBS/src/bbs/BbsDAO.java
+++ BBS/src/bbs/BbsDAO.java
@@ -17,7 +17,7 @@ public class BbsDAO {
 			String dataBaseUrl = "jdbc:mysql://localhost:3306/BBS";
 			String dataBaseId = "root";
 			String dataBasePw = "1234";
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dataBaseUrl,dataBaseId,dataBasePw);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -113,5 +113,29 @@ public class BbsDAO {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	public Bbs getBbs(int bbsID) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				return bbs;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
